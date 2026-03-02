3b4f18bdb02666ca7b2f33657452a304874fb5f1
myName <lea.renaud95710@gmail.com>
2026-03-02 13:10:12 +0100

    Insert test data into habits table

--- Projet_Habit-tracker/Backend/src/main/resources/data.sql
+++ Projet_Habit-tracker/Backend/src/main/resources/data.sql
@@ -1,2 +1,7 @@
--- TODO: Donnees SQL optionnelles.
--- Exemple: INSERT INTO habits (...) VALUES (...);
+-- Création de données de test pour la table habits
+
+INSERT INTO habits (id, name, description, frequency) VALUES
+(1, 'Lire 30 min', 'Lire un livre ou article chaque jour', 'Quotidien'),
+(2, 'Faire du sport', '30 min de sport ou yoga', 'Quotidien'),
+(3, 'Méditation', '5-10 min de méditation', 'Quotidien'),
+(4, 'Boire de l’eau', 'Au moins 2 litres d’eau par jour', 'Quotidien');
