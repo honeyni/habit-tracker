e1c1541fdae7c2031f5a1028b787ca653e0fa709
myName <lea.renaud95710@gmail.com>
2026-03-02 13:39:54 +0100

    Update CheckInCreateRequest.java

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/dto/request/CheckInCreateRequest.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/dto/request/CheckInCreateRequest.java
@@ -1,6 +1,24 @@
 package com.example.habittracker.dto.request;
 
+import java.time.LocalDate;
+
 public class CheckInCreateRequest {
-    // TODO: Champs d'entree pour creer un check-in.
-    // Exemple: habitId, date
+
+    private Long habitId;
+    private LocalDate date;
+
+    // Constructeurs
+    public CheckInCreateRequest() {}
+
+    public CheckInCreateRequest(Long habitId, LocalDate date) {
+        this.habitId = habitId;
+        this.date = date;
+    }
+
+    // Getters et setters
+    public Long getHabitId() { return habitId; }
+    public void setHabitId(Long habitId) { this.habitId = habitId; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
 }
