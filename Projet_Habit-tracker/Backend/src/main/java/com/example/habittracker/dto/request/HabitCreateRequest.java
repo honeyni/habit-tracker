832ecaf7508bc4584401bc339b06fdcc78ae28c4
myName <lea.renaud95710@gmail.com>
2026-03-02 13:43:48 +0100

    Update HabitCreateRequest.java

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/dto/request/HabitCreateRequest.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/dto/request/HabitCreateRequest.java
@@ -1,6 +1,29 @@
 package com.example.habittracker.dto.request;
 
+import com.example.habittracker.util.HabitType;
+
 public class HabitCreateRequest {
-    // TODO: Champs d'entree pour creer une habitude.
-    // Exemple: name, type, target
+
+    private String name;
+    private String description;
+    private HabitType type;
+
+    // Constructeurs
+    public HabitCreateRequest() {}
+
+    public HabitCreateRequest(String name, String description, HabitType type) {
+        this.name = name;
+        this.description = description;
+        this.type = type;
+    }
+
+    // Getters et setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public HabitType getType() { return type; }
+    public void setType(HabitType type) { this.type = type; }
 }
