eb7ad46759318eab449fc613ec6afb21b4f466b2
honeyni <ninon.matidika@edu.esiee-it.fr>
2026-03-02 13:38:11 +0100

    Add constructors and toString method to WeeklyHabit

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/entity/WeeklyHabit.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/entity/WeeklyHabit.java
@@ -1,5 +1,25 @@
 package com.example.habittracker.entity;
 
+// WeeklyHabit = habitude a faire chaque semaine
 public class WeeklyHabit extends Habit {
-    // TODO: Entite habitude hebdomadaire.
+
+    // Constructeur vide
+    public WeeklyHabit() {
+    }
+
+    // Constructeur pratique
+    public WeeklyHabit(int id, String name, int target) {
+        setId(id);
+        setName(name);
+        setTarget(target);
+    }
+
+    // Affichage simple
+    @Override
+    public String toString() {
+        return "WeeklyHabit{id=" + getId()
+                + ", name='" + getName() + '\''
+                + ", target=" + getTarget()
+                + "}";
+    }
 }
