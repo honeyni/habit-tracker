c8376cb5f5844ed5757e635f7ba6f885d955521a
honeyni <ninon.matidika@edu.esiee-it.fr>
2026-03-02 13:38:33 +0100

    Implement MonthlyHabit class with constructors and toString
    Added constructors and toString method for MonthlyHabit.

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/entity/MonthlyHabit.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/entity/MonthlyHabit.java
@@ -1,5 +1,25 @@
 package com.example.habittracker.entity;
 
+// MonthlyHabit = habitude a faire chaque mois
 public class MonthlyHabit extends Habit {
-    // TODO: Entite habitude mensuelle.
+
+    // Constructeur vide
+    public MonthlyHabit() {
+    }
+
+    // Constructeur pratique
+    public MonthlyHabit(int id, String name, int target) {
+        setId(id);
+        setName(name);
+        setTarget(target);
+    }
+
+    // Affichage simple
+    @Override
+    public String toString() {
+        return "MonthlyHabit{id=" + getId()
+                + ", name='" + getName() + '\''
+                + ", target=" + getTarget()
+                + "}";
+    }
 }
