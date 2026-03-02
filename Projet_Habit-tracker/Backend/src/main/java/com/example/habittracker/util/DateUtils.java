da2c530406b8cb1157dd781b7c8820b0c1e638d4
myName <lea.renaud95710@gmail.com>
2026-03-02 13:22:26 +0100

    Implement date utility methods for week and month
    Added methods to get the start and end of the week and month.

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/util/DateUtils.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/util/DateUtils.java
@@ -1,11 +1,25 @@
 package com.example.habittracker.util;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public final class DateUtils {
 
-    private DateUtils() {
-        // TODO: Classe utilitaire statique.
+    // Constructeur privé pour empêcher l'instanciation
+    private DateUtils() {}
+    public static LocalDate startOfWeek(LocalDate date) {
+        return date.with(DayOfWeek.MONDAY);
     }
 
-    // TODO: Ajouter utilitaires de date.
-    // Exemple: startOfWeek(date), endOfWeek(date)
+    public static LocalDate endOfWeek(LocalDate date) {
+        return date.with(DayOfWeek.SUNDAY);
+    }
+
+    public static LocalDate startOfMonth(LocalDate date) {
+        return date.withDayOfMonth(1);
+    }
+
+    public static LocalDate endOfMonth(LocalDate date) {
+        return date.withDayOfMonth(date.lengthOfMonth());
+    }
 }
