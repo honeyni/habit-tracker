a5c608312cf7871c9a5a6cf8144566f8e69705c4
myName <lea.renaud95710@gmail.com>
2026-03-02 13:45:05 +0100

    Update CheckInResponse.java

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/dto/response/CheckInResponse.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/dto/response/CheckInResponse.java
@@ -1,6 +1,29 @@
 package com.example.habittracker.dto.response;
 
+import java.time.LocalDate;
+
 public class CheckInResponse {
-    // TODO: Champs de sortie d'un check-in.
-    // Exemple: id, habitId, date
+
+    private Long id;
+    private Long habitId;
+    private LocalDate date;
+
+    // Constructeurs
+    public CheckInResponse() {}
+
+    public CheckInResponse(Long id, Long habitId, LocalDate date) {
+        this.id = id;
+        this.habitId = habitId;
+        this.date = date;
+    }
+
+    // Getters et setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getHabitId() { return habitId; }
+    public void setHabitId(Long habitId) { this.habitId = habitId; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
 }
