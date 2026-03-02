be8d04dfadda14290acb75f06ce6441a0354e490
honeyni <ninon.matidika@edu.esiee-it.fr>
2026-03-02 13:35:57 +0100

    Add constructors and toString method to DailyHabit

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/entity/DailyHabit.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/entity/DailyHabit.java
@@ -1,5 +1,25 @@
 package com.example.habittracker.entity;
 
+// DailyHabit = habitude a faire chaque jour
 public class DailyHabit extends Habit {
-    // TODO: Entite habitude quotidienne.
+
+    // Constructeur vide (obligatoire dans beaucoup de cas)
+    public DailyHabit() {
+    }
+
+    // Constructeur pratique pour creer rapidement une habitude quotidienne
+    public DailyHabit(int id, String name, int target) {
+        setId(id);
+        setName(name);
+        setTarget(target);
+    }
+
+    // Methode d'affichage simple (utile pour debug)
+    @Override
+    public String toString() {
+        return "DailyHabit{id=" + getId()
+                + ", name='" + getName() + '\''
+                + ", target=" + getTarget()
+                + "}";
+    }
 }
