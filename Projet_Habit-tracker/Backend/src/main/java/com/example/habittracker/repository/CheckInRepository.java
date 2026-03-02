2ca8d1b32182f8cf8aad65615eadac5e34b84f1b
honeyni <ninon.matidika@edu.esiee-it.fr>
2026-03-02 13:48:56 +0100

    Extend CheckInRepository to extend JpaRepository

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/repository/CheckInRepository.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/repository/CheckInRepository.java
@@ -1,5 +1,15 @@
 package com.example.habittracker.repository;
 
-public interface CheckInRepository {
-    // TODO: Etendre JpaRepository<CheckIn, Long>.
+import com.example.habittracker.entity.CheckIn;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// Repository pour l'entite CheckIn
+@Repository
+public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
+
+    // Methode personnalisée: recuperer les check-ins d'une habitude
+    List<CheckIn> findByHabitId(int habitId);
 }
