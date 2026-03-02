bbb90204e81a9d324bd484bde88551291615866a
honeyni <ninon.matidika@edu.esiee-it.fr>
2026-03-02 15:31:13 +0100

    Update CheckInController.java

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/controller/CheckInController.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/controller/CheckInController.java
@@ -1,6 +1,6 @@
 package com.example.habittracker.controller;
 
-import com.example.habittracker.model.CheckIn;
+import com.example.habittracker.entity.CheckIn;
 import com.example.habittracker.repository.CheckInRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
