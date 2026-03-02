95444f94570f276d8ef1f73c91b7407d6aab938b
honeyni <ninon.matidika@edu.esiee-it.fr>
2026-03-02 13:49:18 +0100

    Extend HabitRepository to use JpaRepository
    Extend HabitRepository to inherit from JpaRepository for CRUD operations.

--- Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/repository/HabitRepository.java
+++ Projet_Habit-tracker/Backend/src/main/java/com/example/habittracker/repository/HabitRepository.java
@@ -1,5 +1,15 @@
 package com.example.habittracker.repository;
 
-public interface HabitRepository {
-    // TODO: Etendre JpaRepository<Habit, Long>.
+import com.example.habittracker.entity.Habit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Repository pour l'entite Habit
+// Integer = type de la cle primaire (id)
+@Repository
+public interface HabitRepository extends JpaRepository<Habit, Integer> {
+
+    // TODO: Ajouter ici des methodes personnalisées si besoin
+    // Exemple:
+    // List<Habit> findByName(String name);
 }
